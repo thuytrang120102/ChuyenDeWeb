266e3a389902c300cb15737e3e7a8de786ebe668
nguyenthithuytrang-20130445 <20130445@st.hcmuaf.edu.vn>
2024-04-01 17:31:57 +0700

    [Homepage] update

--- FE/src/components/FilmCard.jsx
+++ FE/src/components/FilmCard.jsx
@@ -1,9 +1,9 @@
 import { IoTicketOutline } from "react-icons/io5";
 import { CiCircleInfo } from "react-icons/ci";
 import exhuma from "../assets/images/homepage/quatmotrungma.jpg"
-const FilmCard = () => {
+const FilmCard = ({type}) => {
     return (
-        <div className="h-[500px] flex flex-col p-2 gap-1 w-full bg-slate-500">
+        <div className="h-[500px] flex flex-col p-2 gap-1 w-full bg-slate-600">
               <div>
                 <img className="h-180px] w-full rounded-xl" src={exhuma}/>
               </div>
@@ -17,15 +17,22 @@ const FilmCard = () => {
               <h1 className="text-white">Thể loại phim: </h1>
               <h1 className="text-white hover:text-black"> Horror</h1>
               </div>
-              <div className="flex flex-row gap-2">
-              <div className="bg-green-600 flex text-white w-[140px] h-[40px] rounded-md gap-1 p-1">
-              <IoTicketOutline className="text-[20px] m-[6px]"/>
-              <h1 className="p-1 ">Đặt vé ngay</h1>
-              </div>
-              <div className="border-green-600 bg-slate-500 border-[2px] rounded-md grid place-items-center w-[40px] h-[40px] hover:bg-green-400">
-              <CiCircleInfo className="text-[25px] m-1"/>
-              </div>
-              </div>
+                {type === "Phim đang chiếu" ? (
+                     <div className="flex flex-row gap-2">
+                     <div className="bg-green-600 flex text-white w-[140px] h-[40px] rounded-md gap-1 p-1">
+                     <IoTicketOutline className="text-[20px] m-[6px]"/>
+                     <h1 className="p-1 text-[14px] ">Đặt vé ngay</h1>
+                     </div>
+                     <div className="border-green-600 bg-slate-600 border-[2px] rounded-md grid place-items-center w-[40px] h-[40px] hover:bg-green-400">
+                     <CiCircleInfo className="text-[25px] m-1"/>
+                     </div>
+                     </div>)
+                 : (<div className="flex flex-row gap-2">
+                    <div className="border-green-600 bg-slate-600 border-[2px] rounded-md place-items-center flex text-white w-[200px] h-[40px] hover:bg-green-400">
+                    <CiCircleInfo className="text-[25px] m-1"/>
+                    <h1 className="p-1 text-[14px]">THÔNG TIN CHI TIẾT</h1>
+                    </div>
+                 </div>)}
             </div>
     )
     
--- FE/src/pages/Homepage.jsx
+++ FE/src/pages/Homepage.jsx
@@ -3,31 +3,42 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import kong from "../assets/images/homepage/kong.jpg"
 import FilmCard from "../components/FilmCard";
+
+const types = ["Phim đang chiếu", "Phim sắp chiếu"]
+
 const Homepage = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <CategoryBar />
-      <div>
-        <div className="w-full h-[700px]">
+      <div className="flex flex-col gap-5">
+        <div className="w-full">
           <img className="w-full" src={kong}  />  </div>
-        <div className="flex flex-col gap-5 ">
-          <h1 className="text-white text-center text-5xl text-">Phim đang chiếu</h1>
+        <div className="flex flex-col gap-3 ">
+          <h1 className="text-white text-center text-3xl">PHIM ĐANG CHIẾU</h1>
           
           <div className="flex gap-5 w-full px-2">
-            <FilmCard />
-            <FilmCard />
-            <FilmCard />
-            <FilmCard />
-            <FilmCard />
-          
-
+            <FilmCard type={types[0]} />
+            <FilmCard type={types[0]} />
+            <FilmCard type={types[0]} />
+            <FilmCard type={types[0]} />
+            <FilmCard type={types[0]} />
             
-        
           </div>
         </div>
+        <div className="flex flex-col gap-3 ">
+          <h1 className="text-white text-center text-3xl">PHIM SẮP CHIẾU</h1>
           
-        <div className="flex flex-col">phim đang chiếu</div>
+          <div className="flex gap-5 w-full px-2">
+            <FilmCard type={types[1]} />
+            <FilmCard type={types[1]} />
+            <FilmCard type={types[1]} />
+            <FilmCard type={types[1]} />
+            <FilmCard type={types[1]} />
+            
+          </div>
+        </div>
+
       </div>
       <Footer />
     </div>
