7f68bbf974bb75c8e76a1dacf540d8082873f45c
qinhuuuuu <qinhuuuuu@gmail.com>
2024-04-03 23:14:36 +0700

    [Film Detail] Build layout page

--- FE/src/routes/route.js
+++ FE/src/routes/route.js
@@ -1,12 +1,15 @@
+import FilmDetailPage from "../pages/FilmDetailPage";
 import FilmsPage from "../pages/FilmsPage";
 import Homepage from "../pages/Homepage";
 
 const pathName = {
   homepage: "/",
   films: "/films",
+  filmDetail: "/film/:id",
 };
 const routes = [
   { path: pathName.homepage, element: Homepage },
   { path: pathName.films, element: FilmsPage },
+  { path: pathName.filmDetail, element: FilmDetailPage },
 ];
 export default routes;
