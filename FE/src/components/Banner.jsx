b5b9c0bdb3e9f08bc3f492c5cd4f28ee7c0f4d51
nguyenthithuytrang-20130445 <20130445@st.hcmuaf.edu.vn>
2024-04-20 17:11:47 +0700

    slider danh sach phim

--- FE/src/components/Banner.jsx
+++ FE/src/components/Banner.jsx
@@ -6,25 +6,11 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 
-const Banner = () => {
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        speed: 1500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+const Banner = ({settings, children}) => {
+    
     return (
-    <Slider {...settings}>
-        <img className="w-full h-[450px] object-fill" src={kong} alt="" /> 
-        <img className="w-full h-[450px] object-fill" src={thanhGuom} alt="" /> 
-        <img className="w-full h-[450px] object-fill" src={kong} alt="" /> 
-        <img className="w-full h-[450px] object-fill" src={thanhGuom} alt="" /> 
-        <img className="w-full h-[450px] object-fill" src={kong} alt="" /> 
+    <Slider {...settings}>{children}
+       
     </Slider>)
 }
 export default Banner;
