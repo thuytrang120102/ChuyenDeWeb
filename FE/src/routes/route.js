eb06dfd6fa8e1a01e251d863f9f9c4c1871dfb16
nguyenthithuytrang-20130445 <20130445@st.hcmuaf.edu.vn>
2024-04-20 17:56:52 +0700

    add UI login, forgot password, register page

--- FE/src/routes/route.js
+++ FE/src/routes/route.js
@@ -1,6 +1,9 @@
 import FilmDetailPage from "../pages/FilmDetailPage";
 import FilmsPage from "../pages/FilmsPage";
+import ForgotPasswordPage from "../pages/ForgotPasswordPage";
 import Homepage from "../pages/Homepage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import SeatSelectingPage from "../pages/SeatSelectingPage";
 
 const pathName = {
@@ -8,11 +11,17 @@ const pathName = {
   films: "/films",
   filmDetail: "/film/:id",
   seatSelectingPage: "/seatSelecting/",
+  registerPage: "/register/",
+  loginPage: "/login/",
+  forgotPage: "/forgot/",
 };
 const routes = [
   { path: pathName.homepage, element: Homepage },
   { path: pathName.films, element: FilmsPage },
   { path: pathName.filmDetail, element: FilmDetailPage },
   { path: pathName.seatSelectingPage, element: SeatSelectingPage },
+  { path: pathName.registerPage, element: RegisterPage },
+  { path: pathName.loginPage, element: LoginPage },
+  { path: pathName.forgotPage, element: ForgotPasswordPage },
 ];
 export default routes;
